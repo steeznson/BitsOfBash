397588d457044ea5c849ba924ba806e2fccd448f
steeznson <james.stevenson09@hotmail.co.uk>
2018-03-09 12:15:30 +0000

    added storage capacity to status bar

--- statusbar.sh
+++ statusbar.sh
@@ -1,6 +1,7 @@
 while true; do
-	printf '\e[A\e[K\e[A\e[K' # delete previous lines
-	cat /sys/class/power_supply/BAT0/capacity # print battery %		
-	date +%H%M
-	sleep 60
+	printf '\e[A\e[K\e[A\e[K\e[A\e[K'; # delete previous line
+	echo -n '%' && cat /sys/class/power_supply/BAT0/capacity; # battery
+	date +%H%M; # time
+	df -H | grep -vE '^Filesystem|devtmpfs|tmpfs|shm|cgroup_root|none' | awk '{print$4" "$1}'; # disk space left
+	sleep 60;
 done
