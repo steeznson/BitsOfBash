83f2aa7dca498b67ad0a5c83b17086a950208611
steeznson <james.stevenson09@hotmail.co.uk>
2018-03-09 12:52:06 +0000

    improved bar

--- statusbar.sh
+++ statusbar.sh
@@ -1,7 +1,7 @@
 while true; do
-	printf '\e[A\e[K\e[A\e[K\e[A\e[K'; # delete previous line
-	echo -n '%' && cat /sys/class/power_supply/BAT0/capacity; # battery
-	date +%H%M; # time
-	df -H | grep -vE '^Filesystem|devtmpfs|tmpfs|shm|cgroup_root|none' | awk '{print$4" "$1}'; # disk space left
+	printf '\e[A\e[K'; # delete previous line
+	echo -n 'Battery:'$(cat /sys/class/power_supply/BAT0/capacity); # battery
+	echo -n ' | Time:'$(date +%H%M); # time
+	echo -n ' | Capacity:'$(df -H | grep -vE '^Filesystem|devtmpfs|tmpfs|shm|cgroup_root|none' | awk '{print$4 " " $1}'); # disk space left
 	sleep 60;
 done
