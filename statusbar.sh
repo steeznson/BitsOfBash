617ff22832c21deb391446957e9d694e3bfe3121
steeznson <james.stevenson09@hotmail.co.uk>
2018-03-09 13:13:14 +0000

    fixed status bar to clear buffer

--- statusbar.sh
+++ statusbar.sh
@@ -1,5 +1,5 @@
 while true; do
-	printf '\e[A\e[K'; # delete previous line
+	echo -e '\0033\0143'; # clear terminal buffer
 	echo -n 'Battery:'$(cat /sys/class/power_supply/BAT0/capacity); # battery
 	echo -n ' | Time:'$(date +%H%M); # time
 	echo -n ' | Capacity:'$(df -H | grep -vE '^Filesystem|devtmpfs|tmpfs|shm|cgroup_root|none' | awk '{print$4 " " $1}'); # disk space left
