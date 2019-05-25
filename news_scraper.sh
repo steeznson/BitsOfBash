40db7a8fcdeb8f06222a681ac37bb411155603fd
James Stevenson <james.al.stevenson@gmail.com>
2019-05-25 13:09:59 +0100

    250519

--- news_scraper.sh
+++ news_scraper.sh
@@ -1,12 +1,2 @@
 #!bin/bash
-while true; do
-    java -jar ~/programming/bbc_scraper.jar # my java news scraper
-    echo -n "Read another story? (y) "
-    read DECISION;
-    if [[ "$DECISION" == "n" ]]; then
-        break # stop if user exits
-    fi
-    if [[ -z "$DECISION" ]]; then
-        break # stop if no decision
-    fi
-done
+curl http://feeds.bbci.co.uk/news/rss.xml?edition=int | grep -o --color=never "\[CDATA\[.*\]\]"
